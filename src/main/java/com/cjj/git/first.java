42d9cacb0295267df3446c2137d1b65a8e9a94e1
2602087227 <108343427+2602087227@users.noreply.github.com>
2022-07-13 17:15:23 +0800

    pull commit

--- src/main/java/com/cjj/git/first.java
+++ src/main/java/com/cjj/git/first.java
@@ -10,5 +10,6 @@ public class first {
         System.out.println("master!");
         System.out.println("fix-hot!");
         System.out.println("push!");
+        System.out.println("pull!");
     }
 }
