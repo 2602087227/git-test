8b94e060e20b713750d4721c9c9fe9979938f2e6
ChenJunjie <2602087227@qq.com>
2022-07-13 15:49:40 +0800

    second commit

--- src/main/java/com/cjj/git/first.java
+++ src/main/java/com/cjj/git/first.java
@@ -3,5 +3,6 @@ package com.cjj.git;
 public class first {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
