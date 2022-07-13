97fa902a013c3e7279b2867b6dc219baef506be8
ChenJunjie <2602087227@qq.com>
2022-07-13 16:23:01 +0800

    hot-fix1 commit

--- src/main/java/com/cjj/git/first.java
+++ src/main/java/com/cjj/git/first.java
@@ -6,5 +6,6 @@ public class first {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hello git5!");
     }
 }
